4ce3ae811311f7169706cb254dea82b3b46843be
Siavee-uni <59541564+Siavee-uni@users.noreply.github.com>
2020-03-28 17:43:29 +0100

    remove email script

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -517,67 +517,11 @@
     </div>
   </div>
   <!-- E-mail script------------------------------------------->
-<?php
-if(isset($_POST['submit'])) {
- 
-    $email_to = "gloehner@gmx.net";
-    $email_subject = "FamilyShare Kontaktformular";
 
-    // validation expected data exists
-    if(!isset($_POST['name']) ||
-        !isset($_POST['email']) ||
-        !isset($_POST['subject']) ||
-        !isset($_POST['message'])) {
-        died('We are sorry, but there appears to be a problem with the form you submitted.');       
-    }
  
-    $name = $_POST['name']; // required
-    $mail = $_POST['email']; // required
-    $subject = $_POST['subject']; // required // not required
-    $comments = $_POST['message']; // required
+<!-- <div id="email">Your message has been sent. Thank you!</div> -->
  
- 
-  if(!preg_match($email_exp,$email_from)) {
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-  }
- 
-    $string_exp = "/^[A-Za-z .'-]+$/";
- 
-  if(!preg_match($string_exp)) {
-    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
-  }
- 
-  if(strlen($comments) < 2) {
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
-  }
- 
-  if(strlen($error_message) > 0) {
-    died($error_message);
-  }
- 
-    $email_message = "Form details below.\n\n";
- 
-     
-    function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
-    }
- 
-    $email_message .= "First Name: ".clean_string($name)."\n";
-    $email_message .= "Email: ".clean_string($mail)."\n";
-    $email_message .= "Subject: ".clean_string($subject)."\n";
-    $email_message .= "Comments: ".clean_string($comments)."\n";
- 
-// create email headers
-$headers = 'From: '.$mail;
-@mail($email_to, $subject, $email_message, $headers);  
 
-header("Location: posts?mailsend")
-?>
- 
-<!-- include your own success html here -->
- 
-<div id="email">Your message has been sent. Thank you!</div>
  
 </section><!-- #contact -->
 
